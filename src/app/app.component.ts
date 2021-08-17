7f7c1171723c1f1de4a4ae30f2d54a7866c16bbe
Rafaela <rn-15@hotmail.com>
2021-08-17 12:02:55 -0300

    Inital commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  public yesNoAnswer = 'no';
+  title = 'a11y-p1';
+}
---
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -0,0 +1,30 @@
+import { Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yes-no-button-group',
+  templateUrl: './yes-no-button-group.component.html',
+  styleUrls: ['./yes-no-button-group.component.scss']
+})
+export class YesNoButtonGroupComponent implements OnInit {
+
+  @Input() public value: string = null;
+  @Input() public label = '';
+  public options = YesNoButtonGroupOptions;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  public activate(value: string): void {
+    this.value = value;
+  }
+}
+
+
+enum YesNoButtonGroupOptions {
+  YES = 'yes',
+  NO = 'no'
+}
