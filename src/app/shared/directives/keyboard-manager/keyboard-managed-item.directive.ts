eb1fff83ce59ea9b1f73935e6185ca8d8372b53b
Rafaela <rn-15@hotmail.com>
2021-08-17 21:20:40 -0300

    Criando ContentChildren com outras diretivas

---
+++ src/app/shared/directives/keyboard-manager/keyboard-managed-item.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appKeyboardManagedItem]'
+})
+export class KeyboardManagedItemDirective {
+
+  constructor() { }
+
+}
--- src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
@@ -1,4 +1,6 @@
+import { ContentChildren, QueryList } from '@angular/core';
 import { Directive, HostListener } from '@angular/core';
+import { KeyboardManagedItemDirective } from './keyboard-managed-item.directive';
 
 @Directive({
   selector: '[appKeyboardManager]'
@@ -7,10 +9,12 @@ export class KeyboardManagerDirective {
 
   constructor() { }
 
+  @ContentChildren(KeyboardManagerDirective) public items: QueryList<KeyboardManagedItemDirective> = null;
   @HostListener('keyup', ['$event'])
   public manageKeys(event: KeyboardEvent): void {
     switch (event.key) {
       case 'ArrowUp':
+        console.log(this.items);
         console.log('pra cima');
         break;
       case 'ArrowDown':
